5640886a141a15ca1cc707287a6757ceab91feb6
eschuman2026-commits <eschuman2026@crossroadscollegeprep.org>
2026-04-30 12:01:46 -0500

    Revert "test"
    This reverts commit c80f1b6548538b51722de07d640a1b488d4c408f.

--- game/main_menu.gd
+++ game/main_menu.gd
@@ -1,11 +1,6 @@
 extends Control
 
-@export var current_level: PackedScene
-
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(current_level)
-	$CanvasLayer/Button.hide()
-	
-	#AllwaysActive.current_level = AllwaysActive.start_level.instantiate()
-	#get_tree().get_root().add_child(AllwaysActive.current_level)
-	#$CanvasLayer.hide()
+	AllwaysActive.current_level = AllwaysActive.start_level.instantiate()
+	get_tree().get_root().add_child(AllwaysActive.current_level)
+	$CanvasLayer.hide()
